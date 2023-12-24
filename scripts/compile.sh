4a34965b1e21b662c66a67fea7f7d133900167a4
nlok5923 <nlokhande5923@gmail.com>
2023-12-24 19:12:22 +0530

    added install and compile scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # deleting existing content
+echo "Deleting existing content..."
 rm -rf ../wasm/* 
 
 cd package
 
+echo "Compiling to wasm..."
 # compile ts to wasm
 npm run compile
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "Build RISC0 prpject..."
 # build your rust project
 cargo build
 
 # getting to the package path
 cd package
 
+echo "Installing node packahes..."
 # installing packages
 npm install
